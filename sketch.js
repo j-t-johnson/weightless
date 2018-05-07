5944b3b6947b76cb9e308c18a0955762d7a3c14d
~ jacob t johnson ~ <jacob@s-MacBook-Pro.local>
2018-05-06 20:01:07 -0400

    arcs

--- sketch.js
+++ sketch.js
@@ -1,48 +1,28 @@
 var number = 0;
 var clickerCount = 0;
 var ramp = 0;
-
-var meow = [];
+var wave = [];
+var retrig;
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
     background(0);
+    loadSign();
 
-    for (var i = 0; i < 2; i++) {
-        meow.push(new Circle(i));
-        meow[i].init();
+    for (var i = 0; i < 128; i++) {
+        wave.push(new Arcs(i));
+        wave[i].init();
     }
-
-    stroke(255);
-    textAlign(CENTER);
-    textFont("Verdana")
-    text("loading...", windowWidth/2, windowHeight/2)
 }
 
 function draw() {
     if (csoundLoaded && clickerCount == 0) {
-        fill(number);
-        stroke(255);
-
-        rectMode(CENTER);
-        rect(windowWidth/2, windowHeight/2, 100, 50);
-        textAlign(CENTER);
-        textFont("Verdana")
-        fill(0);
-        stroke(0);
-        text("render", windowWidth/2, windowHeight/2)
-
-        number = ((number + 0.1) % 100)+ 50;
+        renderSq();
     }
     if (csoundLoaded && clickerCount > 0) {
-
-        noStroke();
-        for (var i = 0; i < meow.length; i++) {
-            meow[i].update();
-            meow[i].draw();
-        }
-        if (ramp < 1) {
-            ramp = ramp + 0.003;
+        for (var i = 0; i < wave.length; i++) {
+            wave[i].update();
+            wave[i].draw();
         }
     }
 }
@@ -62,45 +42,57 @@ function windowResized() {
     background(0);
 }
 
-function Circle() {
+function loadSign() {
+    stroke(255);
+    fill(255);
+    textAlign(CENTER);
+    textFont("Verdana")
+    text("loading...", windowWidth/2, windowHeight/2)
+}
 
-    this.positionX;
-    this.positionY;
+function renderSq() {
+    fill(number);
+    stroke(255);
 
-    this.positionXo;
-    this.positionYo;
-    this.positionXn;
-    this.positionYn;
+    rectMode(CENTER);
+    rect(windowWidth/2, windowHeight/2, 100, 50);
+    textAlign(CENTER);
+    textFont("Verdana")
+    fill(0);
+    stroke(0);
+    text("render", windowWidth/2, windowHeight/2)
 
-    this.stepx = (this.positionXn - this.positionXo)/128;
-    this.stepy = (this.positionYn - this.positionYo)/128;
+    number = ((number + 0.1) % 100)+ 50;
+}
 
-    this.number = Math.floor(random(0, 128));
+function Arcs(index) {
+    this.index = index;
+    this.offset = this.index * 2;
+    this.number = 0 - this.offset;
+    this.angle;
+    this.reposition = function() {
+        this.x = newX + this.index * random(0,5);
+        this.y = newY + this.index * random(0,5);
+    }
     this.init = function() {
-            this.positionXo = random(0, windowWidth);
-            this.positionYo = random(0, windowHeight);
-            this.positionXn = random(0, windowWidth);
-            this.positionYn = random(0, windowHeight);
-
-            this.stepx = (this.positionXn - this.positionXo)/128;
-            this.stepy = (this.positionYn - this.positionYo)/128;
-
-            this.positionX = this.positionXo;
-            this.positionY = this.positionYo;
-
-
+        this.angle = random(0.5, 2);
     }
     this.update = function() {
-        this.number = (this.number + 1)% 128;
-        this.positionX = this.positionX + this.stepx;
-        this.positionY = this.positionY + this.stepy;
-        if (this.number == 0) {
-            this.init();
-        }
+        this.number += 5;
     }
     this.draw = function() {
-        noFill()
-        stroke((Math.abs(64-this.number)*2)*ramp);
-        arc(this.positionX, this.positionY, (Math.abs(this.number-64)*2), (Math.abs(this.number-64)*2), 0+this.number*0.01, 1.1+this.number*0.1);
+        noFill();
+        if (this.number >= 0 && this.number < 256) {
+            stroke(this.number);
+        } else if (this.number == 500){
+            this.number = 0 - this.offset;
+            if (this.index == 0) {
+                newX = random(0, windowWidth);
+                newY = random(0, windowHeight);
+            }
+        } else {
+            stroke(0);
+        }
+        arc(100 + this.index, 100 + this.index, this.index*2, this.index*2, this.index*0.05, this.angle+this.index*0.05);
     }
 }
